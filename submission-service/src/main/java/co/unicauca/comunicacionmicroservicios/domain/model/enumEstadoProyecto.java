8cf0e6e2a9efe7b337456142dd2d8c9e60bf2a24
derlygomezvl <derlygomezvl@unicauca.edu.co>
2025-10-16 20:51:56 -0500

    Delete submission-service directory

--- submission-service/src/main/java/co/unicauca/comunicacionmicroservicios/domain/model/enumEstadoProyecto.java
+++
@@ -1,6 +0,0 @@
-package co.unicauca.comunicacionmicroservicios.domain.model;
-
-public enum enumEstadoProyecto {
-    EN_PROCESO, APROBADO, RECHAZADO, RECHAZADO_DEFINITIVO
-}
-
--- submission-service/src/main/java/co/unicauca/comunicacionmicroservicios/domain/model/enumProgram.java
+++
@@ -1,8 +0,0 @@
-package co.unicauca.comunicacionmicroservicios.domain.model;
-
-public enum enumProgram {
-    INGENIERIA_DE_SISTEMAS,
-    INGENIERIA_ELECTRONICA_Y_TELECOMUNICACIONES,
-    AUTOMATICA_INDUSTRIAL,
-    TECNOLOGIA_EN_TELEMATICA
-}
